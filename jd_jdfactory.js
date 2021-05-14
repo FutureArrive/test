9c8d7ac635b499bdad54fd4a3ac3070e2e33ae7c
FutureArrive <73518137+FutureArrive@users.noreply.github.com>
2021-05-14 14:31:14 +0800

    Update jd_jdfactory.js

--- jd_jdfactory.js
+++ jd_jdfactory.js
@@ -725,7 +725,9 @@ function TotalBean() {
               $.isLogin = false; //cookie过期
               return
             }
-            $.nickName = data['base'].nickname;
+            if($.nickName){
+              $.nickName = data['base'].nickname;
+            }
           } else {
             console.log(`京东服务器返回空数据`)
           }
